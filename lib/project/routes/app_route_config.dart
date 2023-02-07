09bf6ae56720d98755349ae32fec0441aa7866a1
Rohan Doshi <rohan.doshi02@gmail.com>
2023-02-07 13:31:10 +0530

    Renamed according to Project stucture

--- lib/project/routes/app_route_config.dart
+++ lib/project/routes/app_route_config.dart
@@ -2,6 +2,8 @@ import 'package:go_router/go_router.dart';
 import 'package:pulzion23/project/routes/app_route_constant.dart';
 
 import '../../features/landing_page/ui/landing_page.dart';
+import '../../features/login_page/ui/login_signin.dart';
+import '../../features/login_page/ui/signup.dart';
 import '../../features/profile_page/ui/profile_page.dart';
 
 final GoRouter router = GoRouter(routes: [
@@ -15,5 +17,15 @@ final GoRouter router = GoRouter(routes: [
     path: '/profile',
     name: RouteName.profile,
     builder: (context, state) => const ProfilePage(),
+  ),
+  GoRoute(
+      path: '/login',
+      name: RouteName.login,
+      builder: (context, state) => const Loginscreen(),
+  ),
+  GoRoute(
+      path: '/signup',
+      name: RouteName.signup,
+      builder: (context, state) => const SignUp(),
   ),
 ]);
--- lib/project/routes/app_route_constant.dart
+++ lib/project/routes/app_route_constant.dart
@@ -1,4 +1,6 @@
 class RouteName {
   static const String landing = 'landing';
   static const String profile = 'profile';
+  static const String login = "login";
+  static const String signup = "signup";
 }
