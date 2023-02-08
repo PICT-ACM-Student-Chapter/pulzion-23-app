dfeca76a7a33bc5650b02f63d1b1a4608a1d4eb2
MokshadVaidya <mokshad.vaidya.1997@gmail.com>
2023-02-08 17:10:17 +0530

    event page Tarun

--- lib/features/landing_page/ui/home_page.dart
+++ lib/features/landing_page/ui/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -13,6 +14,31 @@ class _HomePageState extends State<HomePage> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: h / 10,
+                  color: Colors.lightBlueAccent,
+                  child: Text("ajshajshajshadfdeddsdjhsha"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: h / 10,
+                  color: Colors.lightGreen,
+                  child: Text("hshakjskhjashakjsh"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Container(
           color: Colors.black,
@@ -21,101 +47,78 @@ class _HomePageState extends State<HomePage> {
               Stack(
                 children: [
                   Container(
-                    height: h/2,
-                      child: Lottie.asset("assets/images/space.json",fit: BoxFit.fill)),
+                      height: h / 2,
+                      child: Lottie.asset("assets/images/space.json",
+                          fit: BoxFit.fill)),
                   Container(
-                    height: h/2,
+                    height: h / 2,
                     decoration: BoxDecoration(
-                      gradient: LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: [Colors.deepPurple.withOpacity(0.3),Colors.black])
-                    ),
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                          Colors.deepPurple.withOpacity(0.3),
+                          Colors.black
+                        ])),
                   ),
                 ],
               ),
-
               Column(
                 children: [
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child:Container(
-
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            height: h/10,
-                            color: Colors.pinkAccent,
-                            child: Text("sjhhsgshsgahsgjghjhjhajsjh"),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            height: h/10,
-                            color: Colors.green,
-                            child: Text("sdhgudhdjahasjahhjhj"),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-              ),
-                  Padding(padding: const EdgeInsets.all(15.0),
-                      child:Container(
-                        height: h/10,
-                        width: w,
-                        color: Colors.grey,
-                        child: Text("Description......."),
-                      ),
-                  ),
-                  Padding(padding: const EdgeInsets.all(15.0),
-                    child:Container(
-                      height: h/10,
-                      width: w,
-                      color: Colors.red,
-                      child: Text("kuch toh"),
-                    ),
-                  ),
-                  Padding(padding: const EdgeInsets.all(15.0),
-                    child:Container(
-                      height: h/10,
-                      width: w,
-                      color: Colors.grey,
-                      child: Text("Description......."),
-                    ),
-                  ),
-                  Padding(padding: const EdgeInsets.all(8.0),
-                    child:Container(
-
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
                       child: Row(
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Container(
-                              height: h/10,
-                              color: Colors.lightBlueAccent,
-                              child: Text("ajshajshajshadfdeddsdjhsha"),
+                              height: h / 10,
+                              color: Colors.pinkAccent,
+                              child: Text("sjhhsgshsgahsgjghjhjhajsjh"),
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Container(
-                              height: h/10,
-
-                              color: Colors.lightGreen,
-                              child: Text("hshakjskhjashakjsh"),
+                              height: h / 10,
+                              color: Colors.green,
+                              child: Text("sdhgudhdjahasjahhjhj"),
                             ),
                           ),
                         ],
                       ),
                     ),
                   ),
-
-
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      height: h / 14,
+                      width: w,
+                      color: Colors.grey,
+                      child: Text("Description......."),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      height: h / 14,
+                      width: w,
+                      color: Colors.red,
+                      child: Text("kuch toh"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      height: h / 14,
+                      width: w,
+                      color: Colors.grey,
+                      child: Text("Description......."),
+                    ),
+                  ),
                 ],
               ),
-
-
-
             ],
           ),
         ),
