ce48fed7fab2e677f1f67f892df881cd2e469360
MokshadVaidya <mokshad.vaidya.1997@gmail.com>
2023-02-08 16:05:44 +0530

    chang

--- lib/features/landing_page/ui/home_page.dart
+++ lib/features/landing_page/ui/home_page.dart
@@ -13,41 +13,79 @@ class _HomePageState extends State<HomePage> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: ListView(
-        children: [
-          Stack(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Column(
             children: [
-              Container(
-                height: h/3,
-                  child: Lottie.asset("assets/images/space.json",fit: BoxFit.fill)),
-              Padding(
-                padding: EdgeInsets.only(top: h/4+20),
-                child: Container(
-                  height: h/9,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: [Colors.deepPurple.withOpacity(0.3),Colors.black])
+              Stack(
+                children: [
+                  Container(
+                    height: h/2,
+                      child: Lottie.asset("assets/images/space.json",fit: BoxFit.fill)),
+                  Container(
+                    height: h/2,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(begin: Alignment.topCenter,end: Alignment.bottomCenter,colors: [Colors.deepPurple.withOpacity(0.3),Colors.black])
+                    ),
                   ),
-                ),
+                ],
               ),
+
+              Column(
+                children: [
+                  Padding(padding: const EdgeInsets.all(8.0),
+                  child:Container(
+
+                    child: Row(
+                      children: [
+                        Container(
+                          color: Colors.pinkAccent,
+                        ),
+                        Container(
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ),
+              ),
+                  Padding(padding: const EdgeInsets.all(8.0),
+                      child:Container(
+                        color: Colors.grey,
+                        child: Text("Description......."),
+                      ),
+                  ),
+                  Padding(padding: const EdgeInsets.all(8.0),
+                    child:Container(
+                      color: Colors.red,
+                      child: Text("kuch toh"),
+                    ),
+                  ),
+                  Padding(padding: const EdgeInsets.all(8.0),
+                    child:Container(
+                      color: Colors.deepOrangeAccent,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: Colors.lightBlueAccent,
+                          ),
+                          Container(
+                            color: Colors.lightGreen,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
+
+                ],
+              ),
+
+
+
             ],
           ),
-          Container(
-            height: h/3,
-            color: Colors.black,
-          ),
-          Container(
-            height: h/3,
-            color: Colors.pinkAccent,
-          ),
-          Container(
-            height: h/3,
-            color: Colors.red,
-          ),
-          Container(
-            height: h/3,
-            color: Colors.pinkAccent,
-          ),
-        ],
+        ),
       ),
     );
   }
