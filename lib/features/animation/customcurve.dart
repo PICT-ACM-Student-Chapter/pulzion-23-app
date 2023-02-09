fe7a84cda7b5e04810c9ca49c003e4a0429267b0
TaRun1369 <tarunsantani2003@gmail.com>
2023-02-09 18:26:36 +0530

    animation

---
+++ lib/features/animation/customcurve.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/animation.dart';
+
+class AnimationCurve extends Curve {
+  @override
+  double transformInternal(double t) {
+    var myValue;
+    if (t <= 0.5) {
+      // t=0 => -(0.5 - 0.0)*2 = -1
+      // t=0.1 => -(0.5 - 0.1)*2 = -0.8
+      // t=0.2 => -(0.5 - 0.2)*2 = -0.6
+      // t=0.3 => -(0.5 - 0.3)*2 = -0.4
+      // t=0.4 => -(0.5 - 0.4)*2 = -0.2
+      // t=0.5 => -(0.5 - 0.5)*2 = 0.0
+      myValue = (0.5 - t) * 2;
+    } else {
+      // t=0.51 => -(1 - (0.55*2)) = 0.1
+      // t=0.6 => -(1 - (0.6*2)) = 0.2
+      // t=0.7 => -(1 - (0.7*2)) = 0.4
+      // t=0.8 => -(1 - (0.8*2)) = 0.6
+      // t=0.9 => -(1 - (0.9*2)) = 0.8
+      // t=1 => -(1 - (1*2)) = 1.0
+      myValue = -(1 - (t * 2));
+    }
+    //print(myValue);
+    return myValue;
+  }
+}
---
+++ lib/features/animation/rocket_in_straight_line.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home:MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+
+  MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool a= false;
+
+
+  bool ?_updateState(){
+    setState(() {
+      a = true;
+    });
+  }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Animation',
+        color: Colors.black,
+        home: Scaffold(
+            backgroundColor: Colors.black,
+            body: Center(
+                child:Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+
+
+                      Container(
+                        height: h*0.9,
+                        width: w,
+                        child: Stack(
+
+                            children:[
+                              AnimatedAlign(
+                                  duration: Duration(milliseconds: 2400),
+                                  alignment:
+                                  a == true?Alignment.bottomCenter:Alignment.topCenter,
+
+                                  child: Transform.rotate(
+                                    angle: pi,
+                                    child: Image(
+                                      width: 100,
+                                      height: 100,
+                                      image: AssetImage('images/rocket3.gif'),
+                                    ),
+                                  )
+                              ),
+                            ]
+                        ),
+                      ),
+                      Container(
+                        height: h*0.1,
+                        width: w,
+                        child: FloatingActionButton(onPressed: (){
+                          _updateState();
+                        },backgroundColor: Colors.black,
+                            child: Text("animate")),
+
+                      ),
+                    ]
+                )
+            )
+        )
+    );
+  }
+}
