c859c363090b6c486d81905ee299d259a14132a0
PeeyushKulgude <peeyush.kulgude777@gmail.com>
2023-02-12 15:48:10 +0530

    Animated Background added

---
+++ lib/constants/widgets/animated_background.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:panorama/panorama.dart';
+
+import '../images.dart';
+
+class AnimatedBackground extends StatelessWidget {
+  AnimatedBackground({super.key});
+
+  final SensorControl sensorControl = SensorControl.AbsoluteOrientation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Panorama(
+          sensitivity: 0.4,
+          animSpeed: 0.5,
+          sensorControl: SensorControl.Orientation,
+          child: Image.asset(AppImages.spaceBackground, fit: BoxFit.cover),
+        ),
+        //Scaffold
+      ],
+    );
+  }
+}
