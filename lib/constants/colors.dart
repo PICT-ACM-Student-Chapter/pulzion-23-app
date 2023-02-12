f5dc81c9f66855ff84323514ccf1fb996410ab44
PeeyushKulgude <peeyush.kulgude777@gmail.com>
2023-02-12 15:31:37 +0530

    Animation Improved

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -42,37 +42,5 @@ class AppColors {
       const Color(0xffed71ad).withAlpha(80),
       const Color(0xff7277f1).withAlpha(80),
     ],
-    [
-      const Color(0xff07f49e).withAlpha(80),
-      const Color(0xff42047e).withAlpha(80),
-    ],
-    [
-      const Color(0xffe4b04d).withAlpha(80),
-      const Color.fromARGB(255, 123, 71, 16).withAlpha(80),
-    ],
-    [
-      const Color(0xffebf4f5).withAlpha(80),
-      const Color.fromARGB(255, 87, 95, 107).withAlpha(80),
-    ],
-    [
-      const Color(0xffb15b86).withAlpha(80),
-      const Color(0xff440f50).withAlpha(80),
-    ],
-    [
-      const Color(0xffef745c).withAlpha(80),
-      const Color(0xff34073d).withAlpha(80),
-    ],
-    [
-      const Color(0xff1ed7b5).withAlpha(80),
-      const Color(0xfff0f9a7).withAlpha(80),
-    ],
-    [
-      const Color(0xff6ff7e8).withAlpha(80),
-      const Color(0xff1f7ea1).withAlpha(80),
-    ],
-    [
-      const Color(0xffed71ad).withAlpha(80),
-      const Color(0xff7277f1).withAlpha(80),
-    ],
   };
 }
--- lib/features/landing_page/ui/landing_page_final.dart
+++ lib/features/landing_page/ui/landing_page_final.dart
@@ -1,4 +1,4 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:sizer/sizer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
@@ -45,55 +45,61 @@ class _LandingPageContentState extends State<LandingPageContent> with TickerProv
                     sensorControl: SensorControl.Orientation,
                     child: Image.asset(AppImages.spaceBackground, fit: BoxFit.cover),
                   ),
-                  Scaffold(
-                    appBar: const CustomAppBar(),
-                    backgroundColor: Colors.transparent,
-                    body: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AppTitle(fontSizeFactor: fontSizeFactor),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: DefaultTabController(
-                            length: 2,
-                            child: TabBar(
-                              labelPadding: const EdgeInsets.all(12),
-                              controller: tabBarController,
-                              indicator: const BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                    color: AppColors.loginPageAccent,
+                  Sizer(
+                    builder: ((context, orientation, deviceType) {
+                      return Scaffold(
+                        appBar: const CustomAppBar(),
+                        backgroundColor: Colors.transparent,
+                        body: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppTitle(fontSizeFactor: fontSizeFactor),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: DefaultTabController(
+                                length: 2,
+                                child: TabBar(
+                                  labelPadding: const EdgeInsets.all(12),
+                                  controller: tabBarController,
+                                  indicator: const BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(
+                                        color: AppColors.loginPageAccent,
+                                      ),
+                                    ),
                                   ),
+                                  unselectedLabelColor: AppColors.cardSubtitleTextColor,
+                                  labelColor: AppColors.loginPageAccent,
+                                  tabs: [
+                                    EventType(
+                                        eventType: "Tech Events", fontSizeFactor: fontSizeFactor),
+                                    EventType(
+                                        eventType: "Non-Tech Events",
+                                        fontSizeFactor: fontSizeFactor),
+                                  ],
                                 ),
                               ),
-                              unselectedLabelColor: AppColors.cardSubtitleTextColor,
-                              labelColor: AppColors.loginPageAccent,
-                              tabs: [
-                                EventType(eventType: "Tech Events", fontSizeFactor: fontSizeFactor),
-                                EventType(
-                                    eventType: "Non-Tech Events", fontSizeFactor: fontSizeFactor),
-                              ],
                             ),
-                          ),
+                            Expanded(
+                              child: TabBarView(
+                                physics: const BouncingScrollPhysics(),
+                                controller: tabBarController,
+                                children: [
+                                  EventGridView(state.events.events!
+                                      .where((element) => element.type == "Technical")
+                                      .toList()),
+                                  EventGridView(state.events.events!
+                                      .where((element) => element.type == "Non Technical")
+                                      .toList()),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                        Expanded(
-                          child: TabBarView(
-                            physics: const BouncingScrollPhysics(),
-                            controller: tabBarController,
-                            children: [
-                              EventGridView(state.events.events!
-                                  .where((element) => element.type == "Technical")
-                                  .toList()),
-                              EventGridView(state.events.events!
-                                  .where((element) => element.type == "Non Technical")
-                                  .toList()),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    bottomNavigationBar: const CustomBottomNavigationBar(),
+                        bottomNavigationBar: const CustomBottomNavigationBar(),
+                      );
+                    }),
                   ),
                 ],
               );
@@ -144,8 +150,8 @@ class AppTitle extends StatelessWidget {
       clipBehavior: Clip.none,
       children: [
         Positioned(
-          top: -150,
-          left: 155,
+          top: -20.h,
+          left: 43.w,
           child: Lottie.asset(
             AppImages.ufo,
             height: 300,
