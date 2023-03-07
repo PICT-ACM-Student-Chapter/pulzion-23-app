57122077ee11b612dfbf9b2d74aafd7f9048d31c
Rohan Doshi <rohan.doshi02@gmail.com>
2023-03-07 23:25:01 +0530

    Removed Assets

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -4,13 +4,10 @@ class AppImages {
   static const djAstronaut = 'assets/images/spaceman_dj.json';
   static const loadingAnimation = 'assets/images/loading-animation.json';
   static const astronautWithPlanet = 'assets/images/astronaut_with_planet.json';
-  static const ufo = 'assets/images/ufo.json';
-  static const ufo2 = 'assets/images/ufo2.json';
   static const ufo3 = 'assets/images/ufo3.json';
   static const person = 'assets/images/person.jpg';
   static const rocketButton = 'assets/images/rocket_button.json';
   static const astronautSayingNo = 'assets/images/astronaut_saying_no.json';
-  static const astronautFloating = 'assets/images/astronaut_floating.json';
   static const spaceman = 'assets/images/space_man.json';
   static const spaceman2 = 'assets/images/space_man2.json';
 }
