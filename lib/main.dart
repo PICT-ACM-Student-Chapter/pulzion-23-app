ac2384b71063ef2310c5d2deee58bb4afa77b755
mokshad-cruv <mokshad@cruv.org>
2023-04-18 23:11:35 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -73,14 +73,14 @@ class Pulzion23App extends StatelessWidget {
           create: (context) => CompulsoryUpdateCubit()..needsUpdate(),
         ),
         BlocProvider(
-          create: (context) => CartPageCubit(),
+          create: (context) => CartPageCubit()..loadCart(),
         ),
         BlocProvider(
           create: (context) => GlobalParameterCubit(),
         ),
         BlocProvider(
           create: (context) => EventSlotsCubit(),
-        )
+        ),
       ],
       child: MaterialApp(
         title: 'Pulzion 23',
