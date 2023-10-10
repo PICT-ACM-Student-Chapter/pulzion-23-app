3ee4fbe3324d9ea0d2f2fcea8c8bf8b61d4184e0
Awadhoot <awadhootk6@gmail.com>
2023-10-10 21:36:58 +0530

    added 3d model

--- lib/pages/more_page/ui/suggestion/suggestion_page.dart
+++ lib/pages/more_page/ui/suggestion/suggestion_page.dart
@@ -1,28 +1,28 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import '../../../../features/home_page/ui/wigets/event_card.dart';
-
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class SuggestionPage extends StatefulWidget {
+  const SuggestionPage({super.key});
+
   @override
   State<SuggestionPage> createState() => _SuggestionPageState();
 }
 
 class _SuggestionPageState extends State<SuggestionPage> {
   String swipetext = "Swipe here -->";
-  final List<EventCard> texts = [
-    // "Codelicious",
-    // "Hire Hustle",
-    // "Codex",
-    // "Dextrous",
-    // "Insight",
-    // "Fandom",
-    // "Dataquest",
-    // "Freeze the second",
-    // "Web and App",
-    // "Electroquest",
+  final List<String> texts = [
+    "Codelicious",
+    "Hire Hustle",
+    "Codex",
+    "Dextrous",
+    "Insight",
+    "Fandom",
+    "Dataquest",
+    "Freeze the second",
+    "Web and App",
+    "Electroquest",
   ];
 
   bool isButtonDisabled = false;
@@ -48,8 +48,8 @@ class _SuggestionPageState extends State<SuggestionPage> {
   }
 
   void startSwipeIndicatorAnimation() {
-    if(isSwipingHintVisible != false)
-      swipeTimer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+    if (isSwipingHintVisible) {
+      swipeTimer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
         setState(() {
           swipeIndicatorPosition += 5; // Adjust the speed as needed
           if (swipeIndicatorPosition >= 400) {
@@ -57,17 +57,19 @@ class _SuggestionPageState extends State<SuggestionPage> {
           }
         });
       });
+    }
   }
+
   void stopSwipeIndicatorAnimation() {
     swipeTimer?.cancel();
   }
-  @override
 
+  @override
   void initState() {
     super.initState();
     startSwipeIndicatorAnimation();
-
   }
+
   @override
   void dispose() {
     swipeTimer?.cancel();
@@ -76,92 +78,109 @@ class _SuggestionPageState extends State<SuggestionPage> {
 
   @override
   Widget build(BuildContext context) {
+    // return FlutterLogo();
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Stack(
-                children: [
-                  SizedBox(
-                    height: 400,
-                    child: WebView(
-                      initialUrl: 'https://app.vectary.com/p/5iw03uFSKS1OazHEbkCuS6',
-                      javascriptMode: JavascriptMode.unrestricted,
-                    ),
-                  ),
-                  Positioned(
-                    top: 160,
-                    left: 200,
-                    child: GestureDetector(
-                      onVerticalDragEnd: (_) {
-                        selectRandomTexts();
-                        isSwipingHintVisible = true;
-                      },
-                      child: Container(
-                        width: 150,
-                        height: 25,
-                        color: Colors.transparent,
-                        // alignment: Alignment.center,
-                        // child: Text(
-                        //   'Swipe here',
-                        //   style: TextStyle(
-                        //     fontSize: 18,
-                        //     color: isSwipingHintVisible
-                        //         ? Colors.blue
-                        //         : Colors.transparent,
-                        //   ),
-                        // ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 165,
-                    left: swipeIndicatorPosition,
-                    child: Container(
-                      width:200,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(12.5),
-                      ),
-                      child: Text(swipetext),
-                    ),
-                  ),
-                  Positioned(
-                    child: Container(
-                      color: Colors.transparent,
-                      height: 150,
-                      width: 400,
-                    ),
-                  ),
-                  Positioned(
-                    top: 190,
-                    child: Container(
-                      color: Colors.transparent,
-                      height: 140,
-                      width: 400,
-                    ),
-                  ),
-                ],
+          child: SizedBox(
+            height: 400,
+            width: 300,
+            child: InteractiveViewer(
+              panAxis: PanAxis.vertical,
+              child: ModelViewer(
+                autoRotate: true,
+                rotationPerSecond: "100deg",
+                src: 'assets/3d/cracked_crystal_ball.glb',
               ),
-              Text(
-                'Swipe right to display random texts.',
-                style: TextStyle(fontSize: 18),
-              ),
-              if (selectedTexts.isNotEmpty)
-                Column(
-                  children: selectedTexts
-                      .map((text) => Text(
-                    text,
-                    style: TextStyle(fontSize: 24),
-                  ))
-                      .toList(),
-                ),
-            ],
+            ),
           ),
         ),
+        // Center(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     children: <Widget>[
+        //       Stack(
+        //         children: [
+        //           const SizedBox(
+        //             height: 400,
+        //             child: WebView(
+        //               initialUrl:
+        //                   'https://app.vectary.com/p/5iw03uFSKS1OazHEbkCuS6',
+        //               javascriptMode: JavascriptMode.unrestricted,
+        //             ),
+        //           ),
+        //           Positioned(
+        //             top: 160,
+        //             left: 200,
+        //             child: GestureDetector(
+        //               // onHorizontalDragEnd: (_) {
+        //               // selectRandomTexts();
+        //               // isSwipingHintVisible = true;
+        //               // },
+        //               child: Container(
+        //                 width: 150,
+        //                 height: 25,
+        //                 color: Colors.black.withOpacity(0.7),
+        //                 // alignment: Alignment.center,
+        //                 // child: Text(
+        //                 //   'Swipe here',
+        //                 //   style: TextStyle(
+        //                 //     fontSize: 18,
+        //                 //     color: isSwipingHintVisible
+        //                 //         ? Colors.blue
+        //                 //         : Colors.transparent,
+        //                 //   ),
+        //                 // ),
+        //               ),
+        //             ),
+        //           ),
+        //           Positioned(
+        //             top: 165,
+        //             left: swipeIndicatorPosition,
+        //             child: Container(
+        //               width: 200,
+        //               height: 50,
+        //               decoration: BoxDecoration(
+        //                 color: Colors.transparent,
+        //                 borderRadius: BorderRadius.circular(12.5),
+        //               ),
+        //               child: Text(swipetext),
+        //             ),
+        //           ),
+        //           Positioned(
+        //             child: Container(
+        //               color: Colors.transparent,
+        //               height: 150,
+        //               width: 400,
+        //             ),
+        //           ),
+        //           Positioned(
+        //             top: 190,
+        //             child: Container(
+        //               color: Colors.transparent,
+        //               height: 140,
+        //               width: 400,
+        //             ),
+        //           ),
+        //         ],
+        //       ),
+        //       Text(
+        //         'Swipe right to display random texts.',
+        //         style: TextStyle(fontSize: 18),
+        //       ),
+        //       if (selectedTexts.isNotEmpty)
+        //         Column(
+        //           children: selectedTexts
+        //               .map((text) => Text(
+        //                     text,
+        //                     style: TextStyle(fontSize: 24),
+        //                   ))
+        //               .toList(),
+        //         ),
+        //     ],
+        // ),
+        // ),
       ),
     );
   }
